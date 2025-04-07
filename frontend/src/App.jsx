069d412cf7383eca2c03c4b55115456e5ffffe65
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-04-07 20:31:44 +0530

    Login and Signup pages UI and Theme configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,56 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Login from './pages/Login';
+import { Menu } from 'lucide-react'; // Importing the Menu icon
+import { useState } from 'react';
+import Signup from './pages/Signup';
+// import Profile from './pages/Profile';
+// import Details from './pages/Details';
+import Sidebar from './components/Sidebar';
+import ProtectedRoute from './components/ProtectedRoutes';
+import DarkModeToggle from './components/DarkModeToggle';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const noSidebarPaths = ['/login', '/signup'];
+  const shouldShowSidebar = !noSidebarPaths.includes(location.pathname);
 
   return (
-    <>
-      <div className="text-3xl font-bold underline text-blue-600">
-      Hello TailwindCSS + React! 🚀
+    <div className='relative min-h-screen font-poppins bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100'>
+     <DarkModeToggle/>
+     {/* Mobile hamburger menu */}
+      {shouldShowSidebar && (
+        <button
+          className="fixed top-4 left-4 z-50 md:hidden p-2 bg-gray-200 dark:bg-gray-700 rounded"
+          onClick={() => setIsSidebarOpen(true)}
+        >
+          <Menu size={24} />
+        </button>
+      )}
+      
+      <div className='flex pt-16 md:pt-0'>
+      {shouldShowSidebar && <Sidebar  isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />}
+      <div style={{ flex: 1, padding: '0px' }}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+           {/* 🔥 Protected routes */}
+           <Route path="/profile" element={
+            <ProtectedRoute>
+              {/* <Profile /> */}
+            </ProtectedRoute>
+          } />
+          <Route path="/details" element={
+            <ProtectedRoute>
+              {/* <Details /> */}
+            </ProtectedRoute>
+          } /> 
+        </Routes>
+        </div>
+      </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
