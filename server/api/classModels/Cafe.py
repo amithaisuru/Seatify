0912db7ca11cc33ebc2ca7ef3467a19522fc8e7a
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-04-09 17:54:11 +0530

    Modify signup page to register cafes

---
+++ server/api/classModels/Cafe.py
@@ -0,0 +1,22 @@
+from extensions import db
+from datetime import datetime
+
+class Cafe(db.Model):
+    __tablename__ = 'cafes'  # Table name in your MySQL DB
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    cafe_name = db.Column(db.String(255), nullable=False)
+    location = db.Column(db.Text)
+    contact_number = db.Column(db.String(20))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=True)
+
+
+    # Relationships
+    owner = db.relationship('User', backref=db.backref('cafes', lazy=True))
+    location = db.relationship('Location', backref=db.backref('cafes', lazy=True))
+
+    def __repr__(self):
+        return f"<Cafe {self.cafe_name}>"
