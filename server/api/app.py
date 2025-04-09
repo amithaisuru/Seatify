b91976845fe4972aa82bb8f73abd13bbe38e8a01
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-04-09 23:00:51 +0530

    cunstomerHomePage API backend integration

--- server/api/app.py
+++ server/api/app.py
@@ -3,6 +3,7 @@ from config import Config
 from extensions import db, jwt, cors
 from routes.signup.signup import signup_bp
 from routes.authentication.login import login_bp
+from routes.customerHomepage.customerHomepage import cafes_bp
 # from routes.profile.profile import profile_bp
 
 def create_app():
@@ -20,6 +21,7 @@ def create_app():
     app.register_blueprint(signup_bp)
     app.register_blueprint(login_bp)
     # app.register_blueprint(profile_bp)
+    app.register_blueprint(cafes_bp)
 
     return app
 
