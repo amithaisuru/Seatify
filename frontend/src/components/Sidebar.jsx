6435af9ef9d390b8b84a047e027b39c273dd8790
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-04-10 21:37:32 +0530

    Sidebar modifications

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -77,16 +77,13 @@ function Sidebar({ isOpen, setIsOpen }) {
                 </Link>
               </li>
               </ul>
-
-              
-              
             )}
             {user.user_type === 2 && (
               <li>
                 <Link 
                   to="/details"
                   onClick={() => setIsOpen(false)}
-                  className="text-sm block px-4 py-2 bg-primary-dark text-gray-800 rounded-md hover:bg-gray-50 dark:text-gray-700 dark:hover:bg-primary-dark transition-colors duration-200"
+                  className="text-sm block px-4 mb-2 py-2 bg-primary-dark dark:bg-primary-darker text-gray-800 rounded-md hover:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-800 transition-colors duration-200"
                 >
                   Details
                 </Link>
