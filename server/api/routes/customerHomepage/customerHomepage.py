e57c3276ac43749602722ae72fc1f4f8c639a1eb
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-04-24 22:46:45 +0530

    frontend and backend api call for seat layout data fetching

--- server/api/routes/customerHomepage/customerHomepage.py
+++ server/api/routes/customerHomepage/customerHomepage.py
@@ -32,7 +32,7 @@ def get_cafes():
     except Exception as e:
         return jsonify({"error": "Unexpected error", "message": str(e)}), 500
 
-@cafes_bp.route('/cafes/<int:cafe_id>', methods=['GET'])
+@cafes_bp.route('/cafes/<int:cafe_id>/info', methods=['GET'])
 @jwt_required()
 def get_cafe_by_id(cafe_id):
     try:
@@ -52,6 +52,51 @@ def get_cafe_by_id(cafe_id):
         }
 
         return jsonify({"cafe": cafe_data}), 200
+
+    except SQLAlchemyError as e:
+        return jsonify({"error": "Database error", "message": str(e)}), 500
+
+    except Exception as e:
+        return jsonify({"error": "Unexpected error", "message": str(e)}), 500
+
+# fetch layout details
+@cafes_bp.route('/cafes/<int:cafe_id>/layout', methods=['GET'])
+@jwt_required()
+def get_seats_by_cafe_id(cafe_id):
+    try:
+        cafe = Cafe.query.get(cafe_id)
+        if not cafe:
+            return jsonify({"error": "Cafe not found"}), 404
+
+         # Hardcoded layout
+        tables = [
+            { "x": 100, "y": 80, "label": "T1" },
+            { "x": 300, "y": 80, "label": "T2" }
+        ]
+
+        chairs = [
+            { "x": 80, "y": 60, "label": "C1", "status": "available" },
+            { "x": 150, "y": 60, "label": "C2", "status": "occupied" },
+            { "x": 80, "y": 120, "label": "C3", "status": "available" },
+            { "x": 170, "y": 90, "label": "C13", "status": "available" },
+            { "x": 160, "y": 130, "label": "C4", "status": "available" },
+            { "x": 130, "y": 150, "label": "C5", "status": "occupied" },
+            { "x": 280, "y": 60, "label": "C10", "status": "available" },
+            { "x": 320, "y": 60, "label": "C6", "status": "occupied" },
+            { "x": 360, "y": 80, "label": "C11", "status": "occupied" },
+            { "x": 370, "y": 110, "label": "C9", "status": "occupied" },
+            { "x": 280, "y": 120, "label": "C7", "status": "available" },
+            { "x": 350, "y": 140, "label": "C8", "status": "available" },
+            { "x": 310, "y": 140, "label": "C12", "status": "available" }
+        ]
+
+        # For demonstration, let's assume all seats are available
+
+        return jsonify({
+                    "cafe_id": cafe_id,
+                    "tables": tables,
+                    "chairs": chairs
+                }), 200
 
     except SQLAlchemyError as e:
         return jsonify({"error": "Database error", "message": str(e)}), 500
