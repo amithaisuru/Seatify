8f424cab0f2d15098a75ff187578c1aefd712003
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-05-21 08:45:36 +0530

    Add Cafe Menus into cafeDetails page

--- frontend/src/pages/customer/CafeDetails.jsx
+++ frontend/src/pages/customer/CafeDetails.jsx
@@ -2,9 +2,12 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import Toast from '../../components/Toast'; // Import your Toast component
-// import SeatMap from '../../components/SeatMap';
 import CafeLayout from '../../components/CafeLayout';
 import { BASE_URL } from '../../constants/config';
+
+import image1 from '../../assets/menuImages/menu1.jpg';
+import image2 from '../../assets/menuImages/menu2.jpg';
+import image3 from '../../assets/menuImages/menu3.jpg';
 
 function CafeDetails() {
 const { id } = useParams();  // Get cafe id from URL
@@ -21,31 +24,17 @@ const delayLogout = () => {
         logout();
     }, 2000);
 };
+const [selectedImage, setSelectedImage] = useState(null);
+
+
+const menuImages = [
+  image1,
+    image2,
+    image3,
+  
+]; 
 
 const fetchCafeDetails = async () => {
-    // setCafe({
-    //     cafe_name: "Cafe Example",
-    //     location: { id: 1, name: "Downtown" },
-    //     contact_number: "123-456-7890"
-    // })
-// try {
-//     const response = await fetch(`http://localhost:5000/cafes/${id}`, {
-//     method: 'GET',
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': `Bearer ${token}`
-//     }
-//     });
-//     const data = await response.json();
-
-//     if (response.ok) {
-//     setCafe(data.cafe);
-//     } else {
-//     console.error('Failed to fetch cafe details');
-//     }
-// } catch (error) {
-//     console.error('Error fetching cafe details:', error);
-// }
 try {
     const response = await fetch(`${BASE_URL}/cafes/${id}/info`,{
         method: 'GET',
@@ -84,7 +73,6 @@ try {
     setToast({ show: true, type: 'error', message: 'An error occurred while fetching cafes.' });
     console.error('Error fetching cafes:', error);
   }
-
 };
 
 // fetch cafe layout details
@@ -156,12 +144,50 @@ return (
         {/* <div className="p-6 min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-white"> */}
         <div className="max-w-xl mx-auto bg-white dark:bg-gray-900 shadow-lg rounded-lg p-6">
         <h1 className="text-lg font-bold text-primary-dark mb-4">{cafe.cafe_name}</h1>
-        <p className="text-md mb-2"><strong>Location:</strong> {cafe.location?.name}</p>
-        <p className="text-md mb-2"><strong>Contact:</strong> {cafe.contact_number}</p>
-        <p className="text-md mb-2"><strong>Seats Available:</strong> {cafe.seats_available}</p>
+        <p className="text-md mb-2"><strong>Town          :</strong> {cafe.location?.name}</p>
+        <p className="text-md mb-2"><strong>Contact           :</strong> {cafe.contact_number}</p>
+        <div className='flex'>
+            <p className='text-md mb-2'><strong>Location        :</strong></p><a href="https://www.google.com/maps/dir/Hot+Wok+Restaurant+and+Pub,+A1,+Yakkala/280+Negombo+Rd,+Wattala+11104/@7.0316881,79.9207333,13z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x3ae2fc3c39eb58f5:0xe0f3aaade34ab34a!2m2!1d80.0350581!2d7.0876391!1m5!1m1!1s0x3ae259b446b37ef9:0x15d8c80fa117f982!2m2!1d79.8889913!2d6.9845786?entry=ttu&g_ep=EgoyMDI1MDUxNS4wIKXMDSoASAFQAw%3D%3D"><u>Google Map</u></a>
+        </div>
+        <p className="text-md mb-2"><strong>Seats Available   :</strong> {cafe.seats_available}</p>
+
         {/* </div> */}
     </div> 
     </div>
+
+    {/* Display menus */}
+    <div className="mt-4 ">
+    <h2 className="text-lg font-bold text-primary-dark mb-4">Menu</h2>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 bg-gray-200 dark:bg-gray-800 rounded-md p-4 ">
+        {menuImages.map((src, index) => (
+        <div key={index} className="w-full overflow-hidden rounded-lg shadow-md bg-white dark:bg-gray-800">
+            <img
+            src={src}
+            alt={`Menu ${index + 1}`}
+            onClick={() => setSelectedImage(src)}
+            className="object-cover w-full h-64 sm:h-48 md:h-60 rounded-lg hover:scale-105 transition-transform duration-300"
+            />
+        </div>
+        ))}
+    </div>
+{/* Modal for full-size image */}
+    {selectedImage && (
+    <div
+        className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50"
+        onClick={() => setSelectedImage(null)}
+    >
+        <img
+        src={selectedImage}
+        alt="Full View"
+        className="max-w-full max-h-[90vh] rounded-lg shadow-lg"
+        />
+    </div>
+    )}
+
+
+
+</div>
+
     <div className="mt-6">
         <h2 className="text-lg font-bold text-primary-dark mb-4">Seat Map</h2>
         {/* <div className="flex justify-center">
@@ -172,10 +198,9 @@ return (
     <CafeLayout tables={tables} chairs={chairs} />
 
     </div>
+
 </main>
 </div>
-
-
 {toast.show && (
     <Toast
         type={toast.type}
